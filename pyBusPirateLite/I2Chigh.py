310179539377fe4108151a6c18004d659d33aebf
Francois Gervais <francoisgervais@gmail.com>
2019-04-07 17:38:08 -0400

    Fix I2Chigh:set_byte()

--- pyBusPirateLite/I2Chigh.py
+++ pyBusPirateLite/I2Chigh.py
@@ -59,12 +59,11 @@ class I2Chigh(I2C):
 
     def set_byte(self, i2caddr, addr, value):
         """ Write one byte to address addr """
-        self.send_start_bit()
-        stat = self.bulk_trans(3, [i2caddr << 1, addr, value])
-        self.send_stop_bit()
+        self.start()
+        stat = self.transfer([i2caddr << 1, addr, value])
+        self.stop()
         if stat.find(chr(0x01)) != -1:
             raise IOError("I2C command on address 0x%02x not acknowledged!" % (i2caddr))
-
 
     def command(self, i2caddr, cmd):
         """ Writes one byte command to slave """
